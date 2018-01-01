cf609322308cd20ad5a50e5cc36a200691ec2865
Narazaka <info@narazaka.net>
2018-01-01 12:06:38 +0900

    webpack関係+外し漏れてたやつ外す

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-const config = require("webpack-config-narazaka-ts-js").node;
-
-config.entry["ghost-kernel"] = "./src/lib/ghost-kernel.ts";
-config.output.library = "ghostKernel";
-
-module.exports = config;
