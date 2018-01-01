78af076f8e0309be64c23f8801cc8656debeebe2
Narazaka <info@narazaka.net>
2018-01-01 14:45:20 +0900

    ルーティング全部

--- src/lib/routings.ts
+++ src/lib/routings.ts
@@ -1,6 +1,18 @@
 import { EventRoutingDefiner } from "lazy-event-router";
-import { InformationRouting } from "./controllers";
+import {
+    InformationRouting,
+    KernelOperationRouting,
+    NotifyInformationRouting,
+    TimeEventRouting,
+    VersionRouting,
+    VisibilityRouting,
+} from "./controllers";
 
 export const routings: EventRoutingDefiner[] = [
     InformationRouting,
+    KernelOperationRouting,
+    NotifyInformationRouting,
+    TimeEventRouting,
+    VersionRouting,
+    VisibilityRouting,
 ];
